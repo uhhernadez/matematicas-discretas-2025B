b2c0bdae1e9e4ff4a6081f1ac07a871b6692cf7b
Uriel Haile <perenganito@gmail.com>
2025-10-02 10:36:15 -0600

    Introducción

---
+++ 01_intro.py
@@ -0,0 +1,8 @@
+import turtle
+donatello = turtle.Turtle()
+donatello.speed(5) # 1:slowest, 3:slow, 5:normal, 10:fast, 0:fastest
+donatello.forward(100)
+donatello.right(45)
+donatello.forward(100)
+donatello.right(45)
+donatello.forward(100)
